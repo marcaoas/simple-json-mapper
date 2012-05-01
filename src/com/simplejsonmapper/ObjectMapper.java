de12ff0889a3f8639ec186f04d5b5d9c5f31bc73
marcosbeirigo <marcosbeirigo@gmail.com>
2012-05-01 14:02:33 -0400

    java doc

--- src/com/simplejsonmapper/ObjectMapper.java
+++ src/com/simplejsonmapper/ObjectMapper.java
@@ -1,26 +1,70 @@
-
 package com.simplejsonmapper;
+
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+@SuppressWarnings("unchecked")
 public class ObjectMapper {
-	
-	public <T> T readValue(String jsonString, Class<T> clazz) throws IllegalArgumentException, InstantiationException, IllegalAccessException, JSONException{
-		JSONObject jsonObject = new JSONObject();
-		return readValue(jsonObject, clazz);
+
+	/**
+	 * Maps a json string to an Object.
+	 * @param jsonString The json representation of  the object. 
+	 * @param clazz The return type of the method.  
+	 * @return An object of type <b>clazz</b>.
+	 * @throws JsonMappingException If the json is not parsable or if any error occurred while binding the values to the object. 
+	 */
+	public <T> T map(String jsonString, Class<T> clazz) throws JsonMappingException{
+		JSONObject jsonObject;
+		try {
+			jsonObject = new JSONObject(jsonString);
+		} catch (JSONException e) {
+			throw new JsonMappingException(e);
+		}
+		return map(jsonObject, clazz);
 	}
 	
-	public <T> T readValue(JSONObject jsonObject, Class<T> clazz) throws InstantiationException, IllegalAccessException, IllegalArgumentException, JSONException {
-		T t = clazz.newInstance();
-		Field[] fields = clazz.getDeclaredFields();
-		for (Field field : fields) {
-			String fieldName = getFieldName(field);
-			if (jsonObject.has(fieldName)) {
-				field.setAccessible(true);
-				field.set(t, jsonObject.get(fieldName));
+	public <T> T map(JSONObject jsonObject, Class<T> clazz) throws JsonMappingException{
+		return mapSingleValue(jsonObject, clazz);
+	}
+
+	public <T> T[] mapArray(String jsonString, Class<T> clazz) throws JsonMappingException{
+		JSONArray jsonArray;
+		try {
+			jsonArray = new JSONArray(jsonString);
+		} catch (JSONException e) {
+			throw new JsonMappingException(e);
+		}
+		return mapArray(jsonArray, clazz);
+	}
+
+
+
+	public <T> T[] mapArray(JSONArray jsonArray, Class<T> clazz) throws JsonMappingException {
+		T[] values = (T[]) Array.newInstance(clazz, jsonArray.length());
+		for (int i = 0; i < values.length; i++) {
+			values[i] = mapSingleValue(jsonArray.optJSONObject(i), clazz);
+		}
+		return values;
+	}
+
+	private <T> T mapSingleValue(JSONObject jsonObject, Class<T> clazz) throws JsonMappingException {
+		T t = null;
+		try {
+			t = clazz.newInstance();
+			Field[] fields = clazz.getDeclaredFields();
+			for (Field field : fields) {
+				String fieldName = getFieldName(field);
+				if (jsonObject.has(fieldName)) {
+					field.setAccessible(true);
+					field.set(t, jsonObject.get(fieldName));
+				}
 			}
+		} catch (Exception e) {
+			throw new JsonMappingException(e);
 		}
 		return t;
 	}
--- test/com/simplejsonmapper/ObjectMapperTest.java
+++ test/com/simplejsonmapper/ObjectMapperTest.java
@@ -1,29 +1,31 @@
 package com.simplejsonmapper;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import com.simplejsonmapper.ObjectMapper;
 
 public class ObjectMapperTest {
 
 	ObjectMapper objectMapper = new ObjectMapper();
 
 	@Test
-	public void testSimpletMapping() throws IllegalArgumentException, InstantiationException, IllegalAccessException, JSONException {
-		JSONObject jsonObject = new JSONObject("{\"name\": \"potato\", \"amount\": 10, \"price\": 2.5}");
-		Product product = objectMapper.readValue(jsonObject, Product.class);
+	public void testSimpletMapping() throws JsonMappingException{
+		String jsonString = "{\"name\": \"potato\", \"amount\": 10, \"price\": 2.5}";
+		Product product = objectMapper.map(jsonString, Product.class);
 		assertEquals("potato", product.getName());
 		assertEquals(10, product.getAmount());
 		assertEquals(2.5, product.getPrice(), 0);
 	}
 
 	@Test
-	public void testOAnnotatedMapping() throws JSONException, IllegalArgumentException, InstantiationException, IllegalAccessException {
-		JSONObject jsonObject = new JSONObject("{\"name\": \"potato\", \"amount\": 10, \"retail_price\": 5.5}");
-		Product product = objectMapper.readValue(jsonObject, Product.class);
+	public void testOAnnotatedMapping() throws JsonMappingException{
+		String jsonString = "{\"name\": \"potato\", \"amount\": 10, \"retail_price\": 5.5}";
+		Product product = objectMapper.map(jsonString, Product.class);
 		assertEquals(5.5, product.getRetailPrice(), 0);
+	}
+	
+	@Test
+	public void testCollectionMapping(){
 	}
 
 }
